4dce388c3871eecabe59141e6844141b2a0770dd
Guilherme Simao <guilhermesimaogh@outlook.com>
2021-11-03 17:41:18 -0300

    Upload of Cliente-Servidor TCP application
    Beware, to work properly, execute "ServidorTCP" first then "ClienteTCP" next. 
    Looking to improve this application in a future not so far away.

---
+++ ClienteTCP.java
@@ -0,0 +1,60 @@
+package clientetcp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClienteTCP {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        /* Especificações : 
+             O cliente pode enviar e receber somente uma mensagem por vez, então não espame!(pretendo melhorar isso)
+             Caso responda 4 ou "não", recebe mensagem do servidor e verifica se é igual a "fechando",
+             caso seja, encerrar conexão.
+        */
+        
+        try {
+            // Iniciando Socket para conectar ao Servidor
+            Socket client = new Socket("localhost", 35355);
+            
+            // Laço de repetição para tratar troca de mensagem
+            while (client.isConnected()) {                
+                InputFromServer(client);
+                OutputToServer(client);
+            }
+            
+            // Fechando conexão
+            FecharConexao(client);
+        } catch (IOException e) {
+            System.out.println("\nErro: "+ e.getMessage());
+        }
+    }
+    // Método que trata de pegar a mensagem vinda do Servidor
+    public static void InputFromServer(Socket client) throws IOException{
+        InputStream InFromServer = client.getInputStream();
+        DataInputStream in = new DataInputStream(InFromServer);
+        String mensagem = in.readUTF();
+        
+        System.out.println("\n\nServer: "+mensagem);
+        
+        if (mensagem.contains("Fechando")) {        // Utilizado para encerrar a conexao de imediato
+            FecharConexao(client);
+        }
+    }
+    // Método que trata de mandar mensagem para o Servidor
+    public static void OutputToServer(Socket client) throws IOException{
+        OutputStream ToServer = client.getOutputStream();
+        DataOutputStream out = new DataOutputStream(ToServer);
+                   
+        System.out.print("You: ");
+        out.writeUTF(s.nextLine());
+    }
+    // Método para fechar conexão, puramente opcional mas preferível
+    private static void FecharConexao(Socket client) throws IOException{
+        client.close();
+    }
+}
---
+++ ServidorTCP.java
@@ -0,0 +1,77 @@
+package servidor.tcp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTCP {
+    
+    public static void main(String[] args) throws IOException {
+        try {
+            /* Especificações : 
+                 Servidor deve ecoar os comandos recebidos e perguntar se deseja continuar após as escolhas, exceto opção 4.
+                 Se escolhido 4 ou "não", encerrar conexão. 
+                 Atender somente um cliente.
+            */
+            
+            // Iiniciando o servidor.
+            ServerSocket server = new ServerSocket(35355);
+            System.out.println("Aguardando conexao na porta \033[36m"+server.getLocalPort()+"\033[m...");
+            
+            // Aceitando conexao de cliente
+            Socket client = server.accept();
+            System.out.printf("Cliente conectado!\tIP: \033[36m%1$s\033[m\n\n",client.getInetAddress().getHostName());
+            
+            // Laço de repetição para tratar troca de mensagem, poderia ficar melhor.
+            while (client.isConnected()) {
+                OutputToClient(client,Menu());
+                InputFromClient(client);
+            }
+            
+            // Encerrar conexao
+            FecharConexao(client);
+        
+        } catch (IOException e) {
+            System.out.println("\nErro: "+e.getMessage());
+        }
+    }
+    // Método que Trata de pegar a mensagem vinda do Socket client
+    public static void InputFromClient(Socket client) throws IOException{
+        InputStream FromClient = client.getInputStream();
+        DataInputStream in = new DataInputStream(FromClient);
+        String Msg = in.readUTF();
+        
+        System.out.println("User: " + Msg); // Não é necessário mostrar isso, puramente opcional
+        
+        if (Msg.contains("4")) {            // Caso o client escolha sair diretamente
+            OutputToClient(client, "Comando recebido \033[36m"+Msg+"\033[m\n\t\033[33mFechando Conexao...\033[m");
+            FecharConexao(client);
+        }else{                      
+            OutputToClient(client, "Comando recebido \033[36m"+Msg+"\033[m\n\tDeseja Continuar? S/N");
+            Msg = in.readUTF();
+            if (Msg.toLowerCase().contains("n")) {      //Caso o client escolha sair indiretamente
+                OutputToClient(client, "Comando recebido \033[36m"+Msg+"\033[m\n\t\033[33mFechando Conexao...\033[m");
+                FecharConexao(client);
+            }
+        }
+    }
+    // Método que trata de enviar mensagem para o Socket client
+    public static void OutputToClient(Socket client,String mensagem) throws IOException{
+        OutputStream FromServer = client.getOutputStream();
+        DataOutputStream out = new DataOutputStream(FromServer);
+        
+        out.writeUTF(mensagem);
+    }
+    // Método para impressão do Menu de Opções
+    public static String Menu(){
+        return "\033[36m----- Menu de opcoes -----\033[m\n\t\033[36m1\033[m - Funcao 1\n\t\033[36m2\033[m - Funcao 2\n\t\033[36m3\033[m - Funcao 3\n\t\033[36m4\033[m - Encerrar Conexao\n";
+    }
+    // Método para fechar conexão, opcional mas preferível
+    public static void FecharConexao(Socket client) throws IOException{
+        client.close();
+    }
+}
